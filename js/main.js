b251a2638cbb7d52d3cc081f51004491f0feab93
Aaron Z GUAN <gz378832500@gmail.com>
2018-09-19 14:50:26 +0800

    update

--- js/main.js
+++ js/main.js
@@ -148,24 +148,19 @@
 		$('#navbar a:not([class="external"])').click(function(event){
 			var section = $(this).data('nav-section'),
 				navbar = $('#navbar');
-
 				if ( $('[data-section="' + section + '"]').length ) {
 			    	$('html, body').animate({
 			        	scrollTop: $('[data-section="' + section + '"]').offset().top - 55
 			    	}, 500);
 			   }
-
 		    if ( navbar.is(':visible')) {
 		    	navbar.removeClass('in');
 		    	navbar.attr('aria-expanded', 'false');
 		    	$('.js-colorlib-nav-toggle').removeClass('active');
 		    }
-
-		    event.preventDefault();
-		    return false;
+//		    event.preventDefault();
+//		    return false;
 		});
-
-
 	};
 
 	// Reflect scrolling in navigation
